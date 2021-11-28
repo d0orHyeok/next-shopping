d912dd36fb1287a5d3453ac4c00736c43053a211
JangHyeok Kim <48301140+d0orHyeok@users.noreply.github.com>
2021-11-28 15:06:29 +0900

    Fix: font 적용문제 해결

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,19 @@
 import Head from 'next/head'
+import Axios from 'axios'
 
 export const Home = (): JSX.Element => {
+  const logout = () => {
+    Axios.get('/api/users/logout')
+      .then((res) => {
+        alert(res.data.message)
+      })
+      .catch((error) => {
+        !error.response.data
+          ? alert('실패하였습니다.')
+          : alert(error.response.data.message)
+      })
+  }
+
   return (
     <>
       <Head>
@@ -9,7 +22,7 @@ export const Home = (): JSX.Element => {
 
       <main style={{ height: '100vh' }}>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <button onClick={logout}>로그아웃 테스트</button>
         </h1>
       </main>
     </>
