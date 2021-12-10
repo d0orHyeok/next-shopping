09583285c5a28f3df5bac543855a5a2256c1e943
KimJangHyeok <48301140+d0orHyeok@users.noreply.github.com>
2021-12-10 13:38:37 +0900

    Add: PreNav.tsx, HOME > path1 > path2 형식의 네비게이션

---
+++ components/utils/PreNav/PreNav.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import { withRouter, NextRouter } from 'next/router'
+import styles from './PreNav.module.css'
+import React from 'react'
+
+interface WithRouterProps {
+  router: NextRouter
+  sx?: React.CSSProperties
+}
+
+const PreNav = ({ router, sx }: WithRouterProps) => {
+  const paths = router.pathname.split('/')
+
+  return (
+    <ol className={styles.nav} style={sx}>
+      {paths.map((path, index) => (
+        <li key={path}>
+          <Link href={!index ? '/' : paths.slice(0, index + 1).join('/')}>
+            {!index ? 'HOME' : path.toUpperCase()}
+          </Link>
+        </li>
+      ))}
+    </ol>
+  )
+}
+
+export default withRouter(PreNav)
