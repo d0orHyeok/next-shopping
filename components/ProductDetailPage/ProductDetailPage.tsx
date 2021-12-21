64599a0d4a09ed1f68275ded6de1a104f4f3d1e1
JangHyeok Kim <48301140+d0orHyeok@users.noreply.github.com>
2021-12-22 02:00:32 +0900

    Add: 상품 상세페이지 반응형 css 적용

--- components/ProductDetailPage/ProductDetailPage.tsx
+++ components/ProductDetailPage/ProductDetailPage.tsx
@@ -3,11 +3,14 @@ import classNames from 'classnames/bind'
 const cx = classNames.bind(styles)
 import { IDetailPageProps } from 'pages/product/detail/[pid]'
 import PreMenu from '@components/ProductViewPage/section/PreMenu'
-import React from 'react'
+import React, { useState } from 'react'
 import ProductOrder from './section/ProductOrder'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 
 const ProductDetailPage = ({ productDetail }: IDetailPageProps) => {
   const { product } = productDetail
+
+  const [viewMore, setViewMore] = useState(false)
 
   return (
     <>
@@ -29,14 +32,44 @@ const ProductDetailPage = ({ productDetail }: IDetailPageProps) => {
             <div className={styles.imgBox}>
               <img src={product.image} alt={product.name} />
             </div>
-            <h1 className={styles.title}>{product.name}</h1>
-            <div className={styles.subImgs}>
-              <h2 className={styles.subTitle}>Product View</h2>
+            <div className={cx('product-info')}>
+              <h1 className={styles.title}>{product.name}</h1>
+              <p>{product.description}</p>
             </div>
+            <h2 className={styles.subTitle}>Product View</h2>
+            {!viewMore ? (
+              <button
+                className={styles.moreBtn}
+                onClick={() => setViewMore(true)}
+              >
+                <span>상세정보 더 보기</span>
+                <ExpandMoreIcon />
+              </button>
+            ) : (
+              <>
+                <div className={styles.subImgs}>
+                  {[
+                    product.subImages.length ? (
+                      product.subImages.map((image, index) => (
+                        <img key={index} src={image} alt={image} />
+                      ))
+                    ) : (
+                      <img src={product.image} alt={product.name} />
+                    ),
+                  ]}
+                </div>
+                <h2 className={styles.subTitle}>Detail Check</h2>
+                <div className={styles.detailCheck}>
+                  detail of products, 핏, 신축성, 비침
+                </div>
+              </>
+            )}
           </div>
 
           {/* 상품 주문영역 */}
-          <ProductOrder product={product} />
+          <div className={cx('product-order')}>
+            <ProductOrder product={product} />
+          </div>
         </div>
       </div>
     </>
